2981f258d0b6bb7841fe6837c49eeb3ce4db29ec
Aman <braich_aman3@yahoo.ca>
2026-03-31 22:46:09 -0400

    fixed app background cropping issue

--- src/components/AppBackground.tsx
+++ src/components/AppBackground.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, View, Platform } from "react-native";
 
 const appBackground = require("../../assets/images/entry-bg.jpg");
 
@@ -9,18 +9,38 @@ type Props = {
 
 export default function AppBackground({ children }: Props) {
   return (
-    <ImageBackground
-      source={appBackground}
-      style={{ flex: 1 }}
-      resizeMode="cover"
+    <View
+      style={{
+        flex: 1,
+        minHeight: "100%",
+      }}
     >
+      <ImageBackground
+        source={appBackground}
+        resizeMode="cover"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          width: "100%",
+          height: "100%",
+        }}
+        imageStyle={{
+          width: "100%",
+          height: "100%",
+        }}
+      />
+
       <View
         style={{
           flex: 1,
+          backgroundColor: "rgba(247, 242, 232, 0.10)",
         }}
       >
         {children}
       </View>
-    </ImageBackground>
+    </View>
   );
 }
